bb20ead43916193e1226793df35c35dd674e4da2
lolooo07 <lolooo07@users.noreply.github.com>
2024-05-22 00:00:00 +0000

    fix typo in variable names

--- Code Sandbox/script.js
+++ Code Sandbox/script.js
@@ -125,4 +125,7 @@ const modalCloseButton = document.getElementById('modal-close-button');
 
 const defaultHtml = `<h1>Hello, Sandbox!</h1>\n<p>Start coding here.</p>`;
 const defaultCss = `body {\n  font-family: sans-serif;\n  color: #333;\n}`;
-const defaultJs = `console.log("Welcome to the Code Sandbox!");`;
+const defaultJs = `console.log("Welcome to the Code Sandbox!");`;
+
+function runCode() {
+    const html = htmlCode.value;
